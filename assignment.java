44b91add4634ae401b2e0d417ba41bd222906f6f
39Nitish-Kumar <nk90226645@gmail.com>
2026-02-11 10:52:44 +0530

    first commit

---
+++ assignment.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class assignment {
+ public static void main(String[] args) {
+    // Q.1.
+    /* 
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    int C = sc.nextInt();
+    int average = (A + B + C) / 3;
+    System.out.println("average is :" + average);
+    */
+
+    // Q.2.
+    /* 
+    Scanner sc = new Scanner(System.in);
+    int side = sc.nextInt();
+    int area = (side * side);
+    System.out.println("area of the square is : " + area);
+    */
+
+    // Q.3.
+    /* 
+    Scanner sc = new Scanner(System.in);
+    float pencil = sc.nextFloat();
+    float    pen = sc.nextFloat();
+    float erraser = sc.nextFloat();
+    float total =  pencil + pen + erraser;
+    System.out.println("Bill is: " + total);
+
+    // add on with 18% tax
+    float newtotal = total + (0.18f * total);
+
+    System.out.println("Bill with 18% tax : " + newtotal);
+ */
+   // Q.4.
+
+   byte b = 4;
+   char c = 'a';
+   short s = 512;
+   int i = 1000;
+   float f = 3.14f;
+    double d = 99.9954;
+ double result  = (f * b) + (i % c) - (d * s);
+   System.out.println(d);
+
+ }   
+}
---
+++ javaBasics.java
@@ -0,0 +1,121 @@
+import java.util.*;
+public class javaBasics {
+  public static void main(String[] args) {
+    //System.out.print("Hello world ");
+    //int a = 10;
+    //int b = 20;
+    //System.out.println(a);
+    //System.out.println(b);
+    //String name = "Tony stark";
+    //System.out.println(name);
+    /* 
+    byte b = 8;
+    System.out.println(b);
+    char ch = 'a';
+    System.out.println(ch);
+    boolean var = false;
+    System.out.println(var);
+    float price = 10.5f;
+    System.out.println(price);
+    // long
+    // double
+    short n = 240;
+    System.out.println(n);
+    */ 
+   // code to calculate sum
+
+   /* 
+   int a = 5;
+   int b = 10;
+   int sum = a + b;
+   System.out.println(sum);
+   */
+  
+   // input in java
+
+   //Scanner sc = new Scanner(System.in);
+   //String input = sc.next();
+   //System.out.println(input);
+
+    //String name = sc.nextLine();
+    //System.out.println(name);
+
+    //int number = sc.nextInt();
+    //System.out.println(number);
+
+    //Float price = sc.nextFloat();
+    //System.out.println(price);
+
+    // sum of a&b (input from user)
+    /* 
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = a + b;
+    System.out.println(sum); 
+    */
+
+    // product of a & b
+     /* 
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int product = a * b;
+    System.out.println(product);
+    */
+
+    // Area of a circle
+
+    /* 
+    Scanner sc = new Scanner(System.in);
+    float rad = sc.nextFloat();
+    float area = 3.14f*rad*rad;
+    System.out.println(area);
+    */
+
+     // unary operators
+     /* 
+
+     // pre increment
+
+     int a = 10;
+     int b = ++a;
+     System.out.println(a);
+     System.out.println(b);
+     
+     // post increment
+
+     int a = 10;
+     int b = a++;
+     System.out.println(a);
+     System.out.println(b);
+     
+     // pre decrement
+
+     int a = 10;
+     int b = --a;
+     System.out.println(a);
+     System.out.println(b);
+
+     // post decrement
+
+     int a = 10;
+     int b = a--;
+     System.out.println(a);
+     System.out.println(b);
+     
+    // relational operator
+
+    int a = 10;
+    int b = 10;
+    System.out.println((a==b));
+    */
+   /* 
+   int a = 10;
+   int b = 10;
+   System.out.println((a!=b));
+   */
+  
+  }    
+}
+//boilerplate code
